849d8b4358f4a12ed985ac75b0540b10e9aa129a
Jared Williams <williamsjared62@gmail.com>
2020-06-19 20:13:29 -0400

    Basic Sign Up

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -9,110 +10,101 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {SafeAreaView} from 'react-native';
 
+import * as eva from '@eva-design/eva';
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  ApplicationProvider,
+  Layout,
+  Text,
+  Input,
+  Button,
+} from '@ui-kitten/components';
+import auth from '@react-native-firebase/auth';
 
 declare const global: {HermesInternal: null | {}};
 
 const App = () => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [error, setError] = React.useState('');
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <Layout
+        style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+        <Layout
+          style={{
+            width: '100%',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            backgroundColor: 'coral',
+            height: 100,
+          }}>
+          <Text category="h1">Sign Up</Text>
+        </Layout>
+        <Layout
+          style={{
+            width: '100%',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            height: 100,
+            backgroundColor: 'purple',
+          }}>
+          <Input
+            placeholder="Email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+        </Layout>
+        <Layout
+          style={{
+            width: '100%',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            height: 100,
+            backgroundColor: 'yellow',
+          }}>
+          <Input
+            placeholder="Password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+        </Layout>
+
+        <Layout
+          // eslint-disable-next-line react-native/no-inline-styles
+          style={{
+            width: '100%',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            height: 100,
+            backgroundColor: 'yellow',
+          }}>
+          <Button
+            onPress={() => {
+              auth()
+                .createUserWithEmailAndPassword(email, password)
+                .catch(setError);
+            }}>
+            Sign Up
+          </Button>
+        </Layout>
+
+        <Layout
+          // eslint-disable-next-line react-native/no-inline-styles
+          style={{
+            width: '100%',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            height: 100,
+            backgroundColor: 'yellow',
+          }}>
+          <Text category="h3">{error.code}</Text>
+        </Layout>
+      </Layout>
+    </ApplicationProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;

b84f0e93c55279809bcf9c588f2e68c338c5a643
Jared Williams <williamsjared62@gmail.com>
2020-07-07 01:24:35 -0400

    Added support for new firebase methods

--- Screens/JoinTeam.tsx
+++ Screens/JoinTeam.tsx
@@ -15,6 +15,8 @@ const JoinTeam = ({navigation, route}) => {
   const [joinCode, setJoinCode] = React.useState('');
 
   const next = () => {
+    AuthService.joinTeam(joinCode);
+    AuthService.getSurveys();
     navigation.navigate('Dashboard');
   };
 
